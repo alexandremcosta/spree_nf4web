4265316a7cdda1df8a59e125dc431d02e1909054
Alexandre Costa <alexandremcost@gmail.com>
2014-08-08 13:44:51 -0300

    fix params bug

--- app/controllers/spree/api/nf4web_controller.rb
+++ app/controllers/spree/api/nf4web_controller.rb
@@ -22,16 +22,16 @@ module Spree
       end
       def nfecommerce_params
         {
-          start_date: permitted_params_nfecommerce[:para1],
-          end_date: permitted_params_nfecommerce[:para2],
-          order_number: permitted_params_nfecommerce[:para3]
+          start_date: permitted_params_nfe[:para1],
+          end_date: permitted_params_nfe[:para2],
+          order_number: permitted_params_nfe[:para3]
         }
       end
       def permitted_params_nfe
         params.permit(:para1, :para2, :para3, :para4, :token)
       end
       def filter_by_status(orders)
-        status_id = permitted_params_nfecommerce[:para4]
+        status_id = permitted_params_nfe[:para4]
         if status_id.present?
           state = Nf4web::StateList.new.index_to_state(status_id)
           return orders.where(payment_state: state)
